bf718d91b1bd25cb8852877d40e3e17e6d2473dd
MGallivan00 <54639309+MGallivan00@users.noreply.github.com>
2019-09-14 14:30:41 -0600

    Add files via upload
    With functions

--- Client.py
+++ Client.py
@@ -9,3 +9,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
 print(b'Received', repr(data))
 
+
+
+def fire():
+
+    # ask for coordinates
+    # split the coordinates
+    # add the specific url content
+    coordinates = input("Choose your next coordinates to bomb: ")
+    coordinates = coordinates.split(" ")
+    content = "x=" + coordinates[0] + "&y=" + coordinates[1]
+    print(content)
+    print(type(coordinates[0]))
+
+    # need to send coordinates to server
+
+    return coordinates[0], coordinates[1]
--- Server.py
+++ Server.py
@@ -22,3 +22,33 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+
+
+def result(x, y, board):
+    # need exceptions later
+
+
+    # check if the coordinates match a boat
+    # if it doesn't, print "Miss"
+    # if it does, prints "Hit", replaces the boat letter with a hit 'X'
+    # then checks if there are any more letters of that boat type
+    # (if the boat sunk or not)
+    # if none are found, print Sunk
+    if board[x][y] != '_':
+        print("Hit " + board[x][y] + "!")
+        temp = board[x][y]
+        board[x][y] = 'X'
+        if(not any(temp in sublist for sublist in board)):
+            print("Sunk!")
+
+
+
+    else:
+        print("Miss!")
+
+    # just a pretty way to print the board
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in board]))
+
+
+    # returns the updated board
+    return board
