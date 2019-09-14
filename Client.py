8dde387374693aa7f60f1600f9568cb5d99d620e
MGallivan00 <54639309+MGallivan00@users.noreply.github.com>
2019-09-14 15:11:56 -0600

    Add files via upload

--- Client.py
+++ Client.py
@@ -1,29 +1,32 @@
 import socket
-HOST = '127.0.0.1'
-PORT = 8080
+import sys
+
 
 def fire():
+    # coordinates from command line
 
-    # ask for coordinates
+    HOST = sys.argv[0]
+    PORT = sys.argv[1]
+    xcor = sys.argv[2]
+    ycor = sys.argv[3]
+
     # split the coordinates
     # add the specific url content
-    coordinates = input("Choose your next coordinates to bomb: ")
-    coordinates = coordinates.split(" ")
-    content = "x = " + coordinates[0] + " & y = " + coordinates[1]
-    print(content)
-    print(type(coordinates[0]))
 
+    content = "x=" + xcor + "&y=" + ycor
+    print(content)
+
+    requests.post('https://' + HOST + ":" + PORT + "?" + content)
+
+    # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    #   s.connect((HOST, PORT))
+    #  s.sendall(b'Ready to play?')
+    # data = s.recv(1024)
     # need to send coordinates to server
 
-    return coordinates[0], coordinates[1]
+    return xcor, ycor
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'Ready to play?')
-    data = s.recv(1024)
 
 print(b'Received', repr(data))
 
 x, y = fire()
-
-
