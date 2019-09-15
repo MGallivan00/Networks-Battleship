2458f8688f044d56efda81981530bdc3e8f419da
Julian Rex <33376885+julianryorex@users.noreply.github.com>
2019-09-14 19:13:51 -0600

    Core program works!
    - added functionality where coordinates given by client.py works.
    - cleaned up the code 
    
    Works if the if statement at the end of the main function is commented. Also, what is the post_format variable in reference to?

--- Socket server.py
+++ Socket server.py
@@ -1,28 +1,10 @@
 import socket
 import sys
 
-#check for argument
-if not (sys.argv) :
-    print("Usage 1 argument")
-    exit()
 
-#putting argument into variable
-port_number=sys.argv[1]
-file_board=sys.argv[2]
-
-
-
-#result function
 def result(x, y, board):
-    # need exceptions later
-    result = "null"
 
-    # check if the coordinates match a boat
-    # if it doesn't, print "Miss"
-    # if it does, prints "Hit", replaces the boat letter with a hit 'X'
-    # then checks if there are any more letters of that boat type
-    # (if the boat sunk or not)
-    # if none are found, print Sunk
+    result = "null"
     if board[x][y] != '_':
         result ="Hit " + board[x][y] + "!"
         temp = board[x][y]
@@ -32,49 +14,63 @@ def result(x, y, board):
     else:
         result = "Miss!"
 
-    # just a pretty way to print the board
     print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in board]))
 
-
-    # returns the updated board
     return result
 ##########
 
-print("BattleShip game\n")
+def main():
 
-with open(file_board) as text:
-    board = [list(line.strip()) for line in text]
-print("Here is your board");
-for x in board:
-    print(x)
+    #check for argument
+    if not (sys.argv):
+        print("Usage 1 argument")
+        exit()
 
-##format to send the fire result
-post_format = """POST /test HTTP/1.1
-Host: 127.0.0.1
-Content-Type: application/x-www-form-urlencoded
-Content-Length: 27
-
-field1=value1&field2=value2"""
-letter_o=post_format[28]
-print(letter_o);
-#
+    #putting argument into variable
+    print(sys.argv)
+    port_number=sys.argv[1]
+    file_board=sys.argv[2]
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind (("127.0.0.1", (int(port_number))))
-s.listen(1)
-while 1:
-    connection, address = s.accept()
-    data = connection.recv(99999)
-    #print(data)
-    print(data.decode()[0])
-    if(data.decode()[0] == 'P'):
-        print("Is a post request, supported")
-        print(data.decode())
-        result=result(1, 1, board);
-        print(result);
+    with open(file_board) as text:
+        board = [list(line.strip()) for line in text]
+    print("BattleShip game")
+    print("Here is your board:\n")
+    for x in board:
+        print(x)
+
+    ##format to send the fire result
+    post_format = """POST /test HTTP/1.1
+    Host: 127.0.0.1
+    Content-Type: application/x-www-form-urlencoded
+    Content-Length: 27
+    field1=value1&field2=value2"""
+    letter_o=post_format[28]
+    print(letter_o) # prints ':' ?
+
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind (("127.0.0.1", (int(port_number))))
+    s.listen(1)
+    while 1:
+        connection, address = s.accept()
+        data = connection.recv(99999).decode("utf-8") # receives encoded message and decodes it to data
+        print("Data:", data)
+        if(data[0] == 'P'):
+            print("Request type: POST (supported)")
+            print(data)
+            xcor = int(data[-5:-4])
+            ycor = int(data[-1:])
+            r = result(xcor, ycor, board)
+            print(r)
+
+
         #if(result ==):
 
-    elif(data.decode()[0] == 'G'):
-        print("Is a get request, Not supported")
-    connection.close
+        elif(data[0] == 'G'):
+            print("Is a get request, Not supported")
+        connection.close
+
+
+main()
