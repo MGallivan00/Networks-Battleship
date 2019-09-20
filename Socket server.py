8404353d3881080efd3af8a05f0932a259fb00a2
Julian Rex <julian.rechsteiner@gmail.com>
2019-09-19 22:07:48 -0600

    More interactive! - added init function - added more print statements to be interactive - deleted unnecessary print statements - added comments - added a couple new functions for redundant code

--- Socket server.py
+++ Socket server.py
@@ -5,7 +5,20 @@ import copy
 # TODO: Add comments # Julian
 # TODO: argument that gets the opponent board # Julian
 
-def printboard(board):
+def init(): # checks if the number of arguments is correct
+
+    if (len(sys.argv) == 1):
+        print("Please enter a Port number and a text file.")
+        exit()
+    if(len(sys.argv) == 2):
+        print("Please enter a second argument.")
+        exit()
+    if(len(sys.argv) > 3):
+        print("Please enter only 2 arguments.")
+        exit()
+
+
+def printboard(board): # prints the board on the browser (uses HTML/CSS)
     cont = '<table style="border:1px solid black; width:50%;text-align:center">'
     for i in range(len(board)):
         cont += '<tr style="border:1px solid black;height:50%; text-align:center">'
@@ -17,79 +30,75 @@ def printboard(board):
     cont += "</table>"
     return cont
 
+def pboard(board):
+    for x in board:
+        print(x)
 
+def boatcheck(boat):
+    b = ""
+    if(boat == 'D'):
+        b = "Destroyer"
+    elif(boat == 'B'):
+        b = "Battleship"
+    elif(boat == 'C'):
+        b = "Carrier"
+    elif(boat == 'R'):
+        b = "Cruiser"
+    else:
+        b = "Submarine"
+
+    return b
 
 def result(x, y, board, records):
 
     result = ""
     records[x][y] = 'X'
     if board[x][y] != '_': # if Hit
-        print("Hit " + board[x][y] + "!")
-        result = "hit=1"
         temp = board[x][y]
+        print("The opponent fired and it hit a " + boatcheck(temp) + "!")
+        result = "hit=1"
         board[x][y] = 'X'
 
         if(not any(temp in sublist for sublist in board)): # check if boat sunk
-            print("Sunk!")
+            print("The opponent sunk the " + boatcheck(temp) + "!")
             result += "\&sink=" + temp
     else:
-        print("Miss!")
+        print("The opponent fired and it missed!")
         result = "hit=0"
 
-
-
-
-    print("Here is your board:\n")
-    for x in board:
-        print(x)
-    print("\n")
-    print("Here is the record of the oponent attacks :\n")
-    for x in records:
-        print(x)
-    print("\n")
+    print("\nThis is the current state of your board:\n")
+    pboard(board)
+    print("\nHere is the record of your opponent's attacks:\n")
+    pboard(records)
 
     # print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in records])) # error
 
     return result
-##############
+
 
 def main():
-
-
-    # checks if the number of arguments is correct
-    if (len(sys.argv) == 1):
-        print("Please enter a Port number and a text file.")
-        exit()
-    if(len(sys.argv) == 2):
-        print("Please enter a second argument.")
-        exit()
-    if(len(sys.argv) > 3):
-        print("Please enter only 2 arguments.")
-        exit()
-
+    init()
     # assigning arguments into variable
-    print(sys.argv)
     port_number=sys.argv[1]
     file_board=sys.argv[2]
 
     # import the board from file
     with open(file_board) as text:
         board = [list(line.strip()) for line in text]
-    #create the record of hit (openent board) from the own board layout and empty it
+
+    # create the record of hit (oppenent board) from the own board layout and empty it
     records = copy.deepcopy(board)
     for i in range(len(records)):
         for j in range(len(records[i])):
             records[i][j]="_"
 
 
-    print("BattleShip game")
-    print("Here is your board:\n")
-    for x in board:
-        print(x)
-    print("\n")
-    print("Here is the record of the oponent attacks :\n")
-    for x in records:
-        print(x)
+    print("\n\tWelcome to Battleship! Let's start the game.\n")
+    print("This is the current state of your board:\n")
+    pboard(board)
+    print("\nHere is the record of your opponent's attacks:\n")
+    pboard(records)
+    print("\nWaiting for opponent to fire...\n")
 
 
     # socket internet address family, with TCP. Binds with port number entered
@@ -99,15 +108,13 @@ def main():
     while True:
         connection, address = s.accept()
         request = connection.recv(99999).decode("utf-8") # receives encoded message and decodes it to request
-        print("request:", request)
+
         if(request[0] == 'P'):
-            print("Request type: POST (supported)")
-            print(request)
+            print("(Request type: POST (supported))")
+            # print(request)
             xcor = int(request[-5:-4]) # get xcor and ycor from request message
             ycor = int(request[-1:])
 
-            # will need to revisit these responses:
-            # what does he mean by not formatted correctly?
             if xcor > len(board) or ycor > len(board) or xcor < 0 or ycor < 0:
                 connection.sendall(str.encode("HTTP/1.1 404 Not Found\r\n\n"))
 
@@ -120,22 +127,21 @@ def main():
                 connection.sendall(str.encode(answer))
 
         elif(request[0] == 'G'): # for GET requests
+            print("(Request type: GET (supported))")
             space = request.find(" ", 5) # finds the second space in the request message
             path = request[4:space] # gathers the path with the space index
 
             # show all coordinates that have been fired on (client perspective)
             if (path == "/opponent_board.html"):
-                #cont = "\n".join(['\t'.join([str(cell) for cell in row]) for row in records])
                 cont = printboard(board)
-            # not quite sure if this is correct yet. Requires comformation.
+
             # show current state of the board (server perspective)
             elif (path == "/own_board.html"):
-                #cont = "\n".join(['\t'.join([str(cell) for cell in row]) for row in board])
                 cont = printboard(board)
+
             else:
                 cont = "Path does not exit"
 
-            print("Request type: GET (supported)")
             answer = "HTTP/1.1 200 OK\r\n\n" + cont
             connection.sendall(str.encode(answer))
 
